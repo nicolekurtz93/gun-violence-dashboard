3dbcf35c7c8000b0ade81dc35a6b1d9b0b09a7d6
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-05-16 19:44:33 -0700

    implemented dropdown for selecting year for data

--- gun-violence-dashboard/src/pages/Homepage.js
+++ gun-violence-dashboard/src/pages/Homepage.js
@@ -2,23 +2,31 @@ import React, { useEffect, useState } from "react";
 import USAMap from "react-usa-map";
 import axios from 'axios';
 import states from '../constants/map_constants'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap/dist/css/bootstrap.css';
+import $ from "jquery";
 
 function Homepage() {
     const [avgStateGunViolence, setAvgStateGunViolence] = useState([]);
+    const [yearForData, setYearForData] = useState([])
     let stateAbbreviationConversion = new Map(states);
     const mapOfAverages = new Map();
     const gunLawUrl = 'https://giffords.org/lawcenter/gun-laws/states/'
 
     useEffect(() => {
         async function fetchAvgData() {
-            const url = 'https://datausa.io/api/data/?drilldowns=State&measures=Firearm Fatalities&year=2021';
+            const url = `https://datausa.io/api/data/?drilldowns=State&measures=Firearm Fatalities&year=${yearForData}`;
             return axios.get(url)
                 .then((data) => setAvgStateGunViolence(data.data.data))
                 .catch((error) => console.log(error))
         }
+
         fetchAvgData()
 
-    }, []);
+    }, [yearForData]);
 
     const giveStatesTitles = () => {
         const map = document.querySelector('.us-state-map')
@@ -44,7 +52,7 @@ function Homepage() {
                     let fill = `rgba(240 , 0, 0, ${opacity} )`
                     colorMap[abbreviation] = {
                         fill: fill,
-                        clickHandler: (event) => window.location.href=gunLawUrl+stateName
+                        clickHandler: (event) => window.location.href = gunLawUrl + stateName
                     }
                 }
                 mapOfAverages.set(stateName, st['Firearm Fatalities'].toFixed(4));
@@ -54,19 +62,32 @@ function Homepage() {
         return colorMap
     }
 
+    const handleSelect = (event) => {
+        // yearForData = event.target.value;
+        setYearForData(event.target.value)
+    }
+
     return (
         <>
             <div className="m-3">
                 <h1 className="text-center">Number of deaths due to firearms per 100,000 population in the US</h1>
                 <h2 className="text-center text-muted">Hover over state to see fatality averages</h2>
                 <h2 className="text-center text-muted">Click on state to learn more about its gun laws</h2>
-            </div> 
+            </div>
+            <div className="dropdown d-flex justify-content-center">
+                <select className="form-control w-25" onChange={handleSelect}>
+                    <option className="dropdown-item text-center" href="#" onSelect={handleSelect} >2021</option>
+                    <option className="dropdown-item text-center" href="#" onSelect={handleSelect}>2020</option>
+                    <option className="dropdown-item text-center" href="#" onSelect={handleSelect}>2019</option>
+                    <option className="dropdown-item text-center" href="#" onSelect={handleSelect}>2018</option>
+                </select>
+            </div>
             <div className="d-flex justify-content-center mt-4">
-                <USAMap 
-                customize={formatStates()} 
-                defaultFill = 'rgba(240 , 0, 0, 1)'
-                title='Number of deaths due to firearms per 100,000 population in the US' />
-                
+                <USAMap
+                    customize={formatStates()}
+                    defaultFill='rgba(240 , 0, 0, 1)'
+                    title='Number of deaths due to firearms per 100,000 population in the US' />
+
             </div>
             <div className="container-fluid">
             </div>
