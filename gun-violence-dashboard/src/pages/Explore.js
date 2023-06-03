250f023678f3802e83487f0dc7b4079e99e55355
stevensonmat2 <stevensonmat2@gmail.com>
2023-06-03 16:17:27 -0700

    add gun deaths chart - refactor api code into functions - implement call and chart for gun deaths - add conditional rendering based on data available

--- gun-violence-dashboard/src/pages/Explore.js
+++ gun-violence-dashboard/src/pages/Explore.js
@@ -7,20 +7,24 @@ import { Bar } from 'react-chartjs-2';
 function Explore() {
   const [gunsOwnedData, setGunsOwnedData] = useState([]);
   const [gunsOwnedYears, setGunsOwnedYears] = useState([]);
+  const [gunDeathsData, setGunDeathsData] = useState([]);
+  const [gunDeathsYears, setGunDeathsYears] = useState([]);
   const [country, setCountry] = useState('');
   const [locations, setLocations] = useState([]);
   const [selectedLocation, setSelectedLocation] = useState({ 'value': '1', 'label': 'Afghanistan' });
   const locationsUrl = '/index.php?option=com_api&app=gpodatapage&clientid=306&key=b7bb356715bf99d6d04e75d266d689db&resource=getlocations&format=raw';
+  const gunsOwnedUrl = `index.php?option=com_api&app=gpodatapage&clientid=306&key=b7bb356715bf99d6d04e75d266d689db&resource=getcategorydata&category=number_of_privately_owned_firearms&location_id=${selectedLocation.value}&format=raw`;
+  const gunDeathsUrl = `index.php?option=com_api&app=gpodatapage&clientid=306&key=b7bb356715bf99d6d04e75d266d689db&resource=getcategorydata&category=total_number_of_gun_deaths&location_id=${selectedLocation.value}&format=raw`;
 
-  function formatGunOwnershipData(data) {
-    let formattedData = data.replace(/\{[^}]+\}|\^/g, '');
-    formattedData = formattedData.split(';');
+  function cleanData(data) {
+    let cleanedData = data.replace(/\{[^}]+\}|\^/g, '');
+    cleanedData = cleanedData.split(';');
 
-    return formattedData
+    return cleanedData
 
   }
 
-  useEffect(() => {
+  const fetchLocations = () => {
     axios
       .get(locationsUrl)
       .then((response) => {
@@ -33,18 +37,17 @@ function Explore() {
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  };
 
-  useEffect(() => {
+  const fetchGunDeaths = () => {
     if (selectedLocation) {
-      const gunsOwnedUrl = `index.php?option=com_api&app=gpodatapage&clientid=306&key=b7bb356715bf99d6d04e75d266d689db&resource=getcategorydata&category=number_of_privately_owned_firearms&location_id=${selectedLocation.value}&format=raw`;
-
       axios
-        .get(gunsOwnedUrl)
+        .get(gunDeathsUrl)
         .then((response) => {
-          const formattedData = formatGunOwnershipData(response.data.result.columnValue);
-          let years = formattedData.map(str => str.split(':')[0]);
-          let data = formattedData.map(str => str.split(':')[1]);
+          const cleanedData = cleanData(response.data.result.columnValue);
+
+          let years = cleanedData.map(str => str.split(':')[0]);
+          let data = cleanedData.map(str => str.split(':')[1]);
 
           years = years.filter(str => str !== "" && str !== undefined);
           years = years.map(str => parseInt(str.replace(/[, ]/g, ""), 10));
@@ -52,21 +55,55 @@ function Explore() {
           data = data.filter(str => str !== "" && str !== undefined);
           data = data.map(str => parseInt(str.replace(/[, ]/g, ""), 10));
 
-          setCountry(response.data.result.location);
-          setGunsOwnedYears(years);
-          setGunsOwnedData(data);
+          setGunDeathsYears(years);
+          setGunDeathsData(data);
         })
         .catch((error) => {
           console.error(error);
         });
     }
-  }, [selectedLocation]);
+  }
+
+  const fetchGunsOwned = (location) => {
+    if (location) {
+      axios
+        .get(gunsOwnedUrl)
+        .then((response) => {
+          const cleanedData = cleanData(response.data.result.columnValue);
+          let years = cleanedData.map(str => str.split(':')[0]);
+          let data = cleanedData.map(str => str.split(':')[1]);
+
+          if (!years.includes(':')) {
+            years = years.filter(str => str !== "" && str !== undefined);
+            data = data.filter(str => str !== "" && str !== undefined);
+            years = years.map(str => parseInt(str.replace(/[, ]/g, ""), 10));
+            data = data.map(str => parseInt(str.replace(/[, ]/g, ""), 10));
+          } else {
+            data = []
+          }
+
+          setCountry(response.data.result.location);
+          setGunsOwnedYears(years);
+          setGunsOwnedData(data);
+
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
 
   const handleLocationChange = (selectedOption) => {
     setSelectedLocation(selectedOption);
   };
 
-  const options = {
+  useEffect(() => {
+    fetchLocations();
+    fetchGunsOwned(selectedLocation);
+    fetchGunDeaths();
+  }, [selectedLocation]);
+
+  const gunsOwnedChartOptions = {
     responsive: true,
     plugins: {
       legend: {
@@ -79,21 +116,59 @@ function Explore() {
     },
   };
 
-
-  const data = {
+  const gunsOwnedChartData = {
     labels: gunsOwnedYears,
     datasets: [
       {
-        label: 'Privately Owned Guns',
+        label: 'Number of Privately Owned Guns',
         data: gunsOwnedData,
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
     ],
   };
 
+  const gunDeathsChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'bottom',
+      },
+      title: {
+        display: false,
+      },
+    },
+  };
+
+  const gunDeathsChartData = {
+    labels: gunDeathsYears,
+    datasets: [
+      {
+        label: 'Number of Gun Deaths',
+        data: gunDeathsData,
+        backgroundColor: '#ff0018',
+      },
+    ],
+  };
+
+  function renderedGunsOwnedChart() {
+    if (gunsOwnedData.length >= 1) {
+      return (<Bar options={gunsOwnedChartOptions} data={gunsOwnedChartData}></Bar>);
+    } else {
+      return (<p>No yearly data available. Estimated numbers: {gunsOwnedYears.length || "None available"}</p>)
+    }
+  }
+
+  function renderedGunDeathsChart() {
+    if (gunDeathsData.length >= 1) {
+      return (<Bar options={gunDeathsChartOptions} data={gunDeathsChartData}></Bar>);
+    } else {
+      return (<p>No data available</p>)
+    }
+  }
 
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center m-2'>
+    <div className='d-flex flex-column align-items-center m-4'>
       <h1>Explore By Location</h1>
       <h2>{country}</h2>
       <Select
@@ -104,13 +179,14 @@ function Explore() {
         placeholder="Select a location"
         isSearchable
       />
-      <div className='d-flex justify-content-between'>
-        <div>
+      <div className='d-flex flex-row justify-content-around w-75 mt-4'>
+        <div className=''>
           <h3>Gun Ownership</h3>
-          <Bar options={options} data={data} />
+          {renderedGunsOwnedChart()}
         </div>
-        <div>
+        <div className=''>
           <h3>Gun Deaths</h3>
+          {renderedGunDeathsChart()}
         </div>
       </div>
 
