f0b2d94d42599f409da9996d02e480f2d15fd64f
nicolekurtz93 <nicolekurtzdesign@gmail.com>
2023-06-03 15:05:44 -0700

    created select dropdown

--- gun-violence-dashboard/src/pages/CompareCountries.js
+++ gun-violence-dashboard/src/pages/CompareCountries.js
@@ -1,20 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { fetchCountryId, fetchPrivatelyOwnedFireArms } from './CompareCountries.service';
-import $ from 'jquery'
+import $ from 'jquery';
+import './styles/compareCountries.css'
 
 function CompareCountries() {
     const [countryIds, setCountryIds] = useState([]);
 
     useEffect(() => {
         fetchCountryId().then(result => {
-            setCountryIds(result)
-            console.log(countryIds)
+            setCountryIds(result);
         })
     }, []);
+
+    useEffect(() => {
+        console.log('ids', countryIds)
+        countryIds.forEach((value, key) => {
+            $('#county_ids').append($('<option>', {
+                value: key,
+                text: value
+            }))
+        })
+    }, [countryIds])
     return (
         <>
             <h1>This is the Ranking Page</h1>
-            <div>
+            <div className="container h-100">
                 <select name="county_ids" multiple="multiple" id="county_ids"></select>
             </div>
         </>
